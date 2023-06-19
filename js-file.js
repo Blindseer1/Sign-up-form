7303f93f75c07445b9c5ac875855512e16200947
Vlad <zeulnimeni@gmail.com>
2023-06-19 14:14:21 +0300

    This did not turn out how I expected

--- js-file.js
+++ js-file.js
@@ -1,16 +1,41 @@
-let password=document.querySelector('#pwd').value;
-let passwordCheck=document.querySelector('#pwd-check').value;
-let span=document.querySelector('span');
+let password=document.querySelector('#pwd');
+let passwordCheck=document.querySelector('#pwd-check');
+let errorField=document.querySelector('#error-check');
 let form=document.querySelector('form');
-form.addEventListener('submit',verifyValidity())
+let firstErrorField=document.querySelector('#firstErrorField')
+
 function verifyValidity()
 {
-    let match=true;
-   span.innerHTML=password==passwordCheck ?'Matching':'not macthing';
-   console.log('working')
-}
-password.addEventListener('keyup',()=>{if(password.length!=0)
-    verifyValidity()})
- password.addEventListener('keydown',()=>{if(passwordCheck.length!=0)
-        verifyValidity()})
+   if(password.value!=passwordCheck.value)
+   {
+   errorField.textContent="Passwords do not match";
+   }
+   else
+   errorField.textContent=" ";
 
+}
+
+
+password.addEventListener('input',()=>
+{
+    if(password.value.length!=0)
+    {
+        verifyValidity()
+       
+    }
+    if(password.value.length<6)
+    {
+        firstErrorField.textContent="Please write a passwrod of at least 6 characters"}
+        else
+        firstErrorField.textContent=" ";
+
+    
+    
+})
+passwordCheck.addEventListener('input',()=>
+{
+    if(passwordCheck.value.length!=0)
+    {
+        verifyValidity()
+    }
+})
